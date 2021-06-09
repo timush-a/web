a6979d6cafda945b89d0e33bf9dfecfb8e6fce80
timush-a <smtimush@yandex.ru>
2021-06-09 16:30:11 +0300

    update views

--- routing/views.py
+++ routing/views.py
@@ -4,51 +4,41 @@ from django.views.decorators.http import require_GET, require_POST
 
 @require_GET
 def simple_route(request):
-    return HttpResponse(content='')
+    return HttpResponse()
 
 
-def slug_route(request):
-    return HttpResponse(content=request.path.split('/')[3])
+def slug_route(request, slug):
+    return HttpResponse(slug)
 
 
-def sum_route(request):
-    first_value = int(request.path.split('/')[3])
-    second_value = int(request.path.split('/')[4])
-    result = first_value + second_value
-    return HttpResponse(content=result)
+def sum_route(request, a, b):
+    try:
+        a = int(a)
+        b = int(b)
+    except (ValueError, TypeError):
+        return HttpResponse(status=400)
+
+    return HttpResponse(a + b)
 
 
 @require_GET
 def sum_get_method(request):
     try:
         a = int(request.GET.get('a'))
-    except (TypeError, ValueError):
-        a = None
-
-    try:
         b = int(request.GET.get('b'))
-    except (TypeError, ValueError):
-        b = None
+    except (ValueError, TypeError):
+        return HttpResponse(status=400)
 
-    if not (a and b):
-        return HttpResponse(content='', status=400)
-
-    return HttpResponse(content=(a + b))
+    return HttpResponse(a + b)
 
 
 @require_POST
 def sum_post_method(request):
     try:
         a = int(request.POST.get('a'))
-    except (TypeError, ValueError):
-        a = None
-
-    try:
         b = int(request.POST.get('b'))
-    except (TypeError, ValueError):
-        b = None
+    except (ValueError, TypeError):
+        return HttpResponse(status=400)
 
-    if not (a and b):
-        return HttpResponse(content='', status=400)
+    return HttpResponse(a + b)
 
-    return HttpResponse(content=(a + b))
