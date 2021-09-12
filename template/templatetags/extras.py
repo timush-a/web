59c260b4290998118466e142f207ce47227f8bb2
timush-a <smtimush@yandex.ru>
2021-09-12 18:01:43 +0300

    initial commit

--- template/templatetags/extras.py
+++ template/templatetags/extras.py
@@ -1,1 +1,32 @@
 from django import template
+
+'''
+2.1. Фильтр inc. Необходимо в файле extras.py создать фильтр “inc“ который принимает 2 аргумента:
+1-й - число которое нужно увеличить, 2-й - на сколько нужно увеличить первое число.
+Пример использования фильтра “inc“ представлен в файле template/templates/filters.html
+
+2.2. Тег division. Необходимо в файле extras.py создать тег “division“ (то есть тег для деления),
+который принимает 3 параметра: 1-ый - делимое,  2-ой - делитель,  3-ий — флаг определяющий тип возвращаемого
+значения для результата деления (именованный аргумент to_int).
+Если переданное значение to_int равно False, необходимо выполнить вещественное деление.
+Если передано True результат вещественного деления необходимо привести к целому. Значение to_int по-умолчанию — False.
+
+Обратите внимание, что делимое и делитель целые числа, но передаются в тег в формате string.
+
+Пример использования тега “division“ представлен в файле template/templates/filters.html 
+'''
+register = template.Library()
+
+@register.filter(name='inc')
+def inc(number, increase):
+    return int(number) + int(increase)
+
+@register.simple_tag
+def division(dividend, divider, to_int=False):
+    dividend = int(dividend)
+    divider = int(divider)
+
+    if not to_int:
+        return dividend / divider
+
+    return int(dividend / divider)
--- template/views.py
+++ template/views.py
@@ -1,10 +1,29 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.views.decorators.http import require_http_methods
 
 
-# Create your views here.
-
+@require_http_methods(['GET', 'POST'])
 def echo(request):
-    pass
+    if request.method == 'GET':
+        params = request.GET
+        x_print_statement = request.META.get('HTTP_X_PRINT_STATEMENT')
+    elif request.method == 'POST':
+        params = request.POST
+        x_print_statement = request.POST.get('HTTP_X_PRINT_STATEMENT')
+
+    method = request.method.lower()
+    result_string = ''
+
+    for a in params.items():
+        result_string += f'{a[0]}: {a[1]} '
+
+    if x_print_statement:
+        body = f'{method} {result_string}{x_print_statement}'
+    else:
+        body = f'{method} {result_string}statement is empty'
+
+    return HttpResponse(status=200, content=body)
 
 
 def filters(request):
